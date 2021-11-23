5fd06ffe3169efde7e51530fd096443e186d9cbb
Anna <a.dyakonu85@mail.ru>
2021-11-23 16:00:44 +0300

    java_1 Lesson_5 Домашнее задание

---
+++ src/main/java/com/geekbrains/Person.java
@@ -0,0 +1,83 @@
+package com.geekbrains;
+
+public class Person {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String telephone;
+    private double pay;
+    private int ags;
+
+    public Person(){
+
+    }
+
+
+        public Person(String fullName, String position, String email, String telephone, double pay, int ags){
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.telephone = telephone;
+        this.pay = pay;
+        this.ags = ags;
+    }
+    public  void print(){
+        System.out.println("ФИО: " +fullName + ". Должность: " +position + ". email: "+email+". телефон: " +telephone+ ". зарплата: "+pay+". возрвст: "+ags);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    public int getAgs() {
+        if (ags>40)
+            print();
+
+
+
+        return ags;
+
+    }
+
+    public void setAgs(int ags) {
+        this.ags = ags;
+    }
+}
+
---
+++ src/main/java/com/geekbrains/main.java
@@ -0,0 +1,22 @@
+package com.geekbrains;
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+        Person[] peersArray = new Person[5];
+
+        peersArray[0] = new Person("Иванов Иван", "Программист", "ivanov@mail.ru", "89614712589", 30000, 25);
+        peersArray[1] = new Person("Петров Вася", "Столяр", "лдлдл@mail.ru","89652147895", 20000, 50);
+        peersArray[2] = new Person("Сидоров Илья", "Менеджер","лао@mail.ru","82596147832",40000,45);
+        peersArray[3] = new Person("Петросян Петр","Юморист","тао@mail.ru","86254128956",50000, 36);
+        peersArray[4] = new Person("ушаков Кирилл","Актер","ьлв@mail.ru","82594178936",45000,29);
+        for (Person person:peersArray) {
+            person.getAgs();
+              
+
+        }
+
+    }
+}
